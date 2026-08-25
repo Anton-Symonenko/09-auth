096e0a0bd2bc925dc53702ab1f6b7920283e6d45
Anton-Symonenko <gan4yrnuk24@gmail.com>
2026-08-25 22:45:12 +0200

    done4

--- proxy.ts
+++ proxy.ts
@@ -38,7 +38,7 @@ export async function proxy(request: NextRequest) {
           return NextResponse.redirect(new URL("/", request.url));
         }
         if (isPrivateRoute) {
-          return NextResponse.next({});
+          return NextResponse.next();
         }
       }
     }
